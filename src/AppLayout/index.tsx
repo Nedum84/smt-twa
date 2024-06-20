f16d58e5b5734a51289ce329139963509763e79d
Nelson Odo <nelsonnedum@gmail.com>
2024-06-20 21:55:21 +0100

    fix:

--- src/AppLayout/index.tsx
+++ src/AppLayout/index.tsx
@@ -3,7 +3,7 @@ import BottomTabs from "./BottomTabs";
 
 export default function AppLayout() {
   return (
-    <div className="w-screen h-screen ">
+    <div className="w-screen h-dvh">
       <div className="h-screen flex flex-col max-w-[800px] mx-0">
         <div className="flex-1 w-full">
           <Outlet />
